0f4bd6906ad632567c4a41da00ee2bed329bce17
Daniel Loibl <daniel.loibl@gmail.com>
2021-01-19 17:38:47 +0100

    add a cloneable sample subpackage

---
+++ sample/index.js
@@ -0,0 +1,13 @@
+const { Transbase } = require("transbase");
+
+const transbase = new Transbase({
+  url: "//localhost:2024/sample",
+  user: "tbadmin",
+  password: "",
+});
+
+const resultSet = transbase.query("select * from cashbook");
+
+console.log(resultSet.toArray());
+
+transbase.close();
